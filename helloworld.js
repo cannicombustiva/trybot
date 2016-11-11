40015103fe1a1d72305f734063872f8e386904e7
cannicombustiva <salvatoredifranco8@gmail.com>
2016-11-11 15:00:12 +0100

    First interaction
    I’ll just add single root ‘/’ dialog that responds to any message with “Hello World”.

--- helloworld.js
+++ helloworld.js
@@ -2,3 +2,6 @@ var builder = require('botbuilder')
 
 var connector = new builder.ConsoleConnector().listen()
 var bot = new builder.UniversalBot(connector)
+bot.dialog('/', function (session) {
+    session.send('Hello World')
+})
